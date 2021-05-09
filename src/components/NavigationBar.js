243600d05331029c1ec3360710fc340095eee32b
AlexanderBowers <7319abowers@gmail.com>
2021-05-08 21:32:26 -0700

    did navbar

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,55 @@
+//import Button from 'react-bootstrap/Button';
+import React from 'react';
+//import {NavLink} from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar'
+import {Nav} from 'react-bootstrap'
+//import Search from './Search'
+
+const NavigationBar = (props) => {
+
+  const logout = () => {
+    localStorage.removeItem("token","user")
+    window.location.href = "/"
+  }
+
+  function loggedIn  () {
+    let token = localStorage.getItem("token")
+      if (token) {
+           return(
+            <Navbar bg="transparent" variant="dark" >
+              <Container className="jHeader">
+                <Nav className="mr-auto">
+                  <Nav.Link href="/about">About</Nav.Link>
+                  <Nav.Link href="/profile">Profile</Nav.Link>
+                  <Nav.Link href="/parties">Parties</Nav.Link>
+                  <Nav.Link href="#" onSelect={logout}>Logout</Nav.Link>
+                </Nav>
+              </Container>
+            </Navbar>
+           )
+      } else {
+        return (
+          <Navbar bg="transparent" variant="dark" >
+            <Container className="jHeader">
+            <Nav className="mr-auto">
+                  <Nav.Link href="/about">About</Nav.Link>
+                  <Nav.Link href="/profile">Profile</Nav.Link>
+                  <Nav.Link href="/parties">Parties</Nav.Link>
+                  <Nav.Link href="/login">Login</Nav.Link>
+                </Nav>
+            </Container>
+          </Navbar>
+        )
+      }
+  }
+
+    
+    return (
+      <div>
+        {loggedIn()}
+      </div>
+    )
+}
+
+export default NavigationBar;
